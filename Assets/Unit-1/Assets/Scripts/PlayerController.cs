c50d054c3d3ee1949305132a52c64e945f53a416
Gummy27 <brimir27@outlook.com>
2020-04-24 10:03:45 +0000

    Asset möppu og build af unit 1 er komið upp á github

---
+++ Assets/Unit-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15;
+    private float turnSpeed = 50;
+    private float horizontalInput;
+    private float forwardInput ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hér er tekið inn frá spilara.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Bíllinn er færður áfram.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Bíllinn beygir
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
